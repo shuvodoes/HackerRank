53434303c129bef49ec30930b60d7f52aacc05d3
Shuvo Das <85067722+shuvodoes@users.noreply.github.com>
2026-08-15 01:21:44 +0600

    Implement error handling for division and input

---
+++ Python/Errors and Exceptions/Exceptions.py
@@ -0,0 +1,17 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(input())
+
+for i in range(n):
+    a, b = input().split()
+
+    try:
+        a = int(a)
+        b = int(b)
+        print(a // b)
+
+    except ZeroDivisionError as e:
+        print("Error Code:", e)
+
+    except ValueError as e:
+        print("Error Code:", e)
